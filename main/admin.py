bdbb7850707d34e97625b971b86870acd48a36ee
Daniel <doroev.daniel1@gmail.com>
2022-12-12 12:33:28 +0600

    product comment inlines

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
+
 from .models import Category, Product
+from review.models import Comment, Rating
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class RatingInline(admin.TabularInline):
+    model = Rating
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category' ,'status']
     list_filter = ['category', 'status']
     search_fields = ['title', 'description']
+    inlines = [CommentInline, RatingInline]
 
 
 admin.site.register(Category)
