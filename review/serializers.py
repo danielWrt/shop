2160cf6ac340dbc004c7f48c184315943ea04b4e
Daniel <doroev.daniel1@gmail.com>
2022-12-12 10:44:24 +0600

    serializers + views

---
+++ review/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Comment, Rating
+
+
+class CommentSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class RatingSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Rating
+        fields = '__all__'
