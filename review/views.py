c6813501d82639c90ad018f0ef28795a19192dba
Daniel <doroev.daniel1@gmail.com>
2022-12-13 10:53:10 +0600

    rating

--- review/views.py
+++ review/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import CommentSerializer, RatingSerializer
 from .models import Comment, Rating
@@ -12,7 +14,22 @@ class CommentViewSet(ModelViewSet):
     permission_classes = [IsAuthorOrReadOnly]
 
 
-class RatingViewSet(ModelViewSet):
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+class CreateRatingAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+
+    def post(self, request):
+        user = request.user
+        ser = RatingSerializer(data=request.data, context={'request': request})
+        ser.is_valid(raise_exception=True)
+        product_id = request.data.get('product')
+        
+        if Rating.objects.filter(author=user, product__id=product_id).exists():
+            rating = Rating.objects.get(author=user, product__id=product_id)
+            rating.value = request.data.get('value')
+            rating.save()
+        else:
+            # Rating.objects.create(user)
+            ser.save()
+        
+        return Response(status=201)
