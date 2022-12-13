53aba12cab680744b71db147ea529d53cc12c98e
Daniel <doroev.daniel1@gmail.com>
2022-12-13 11:04:00 +0600

    swagger

--- review/views.py
+++ review/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import CommentSerializer, RatingSerializer
 from .models import Comment, Rating
@@ -17,7 +18,7 @@ class CommentViewSet(ModelViewSet):
 class CreateRatingAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-
+    @swagger_auto_schema(request_body=RatingSerializer())
     def post(self, request):
         user = request.user
         ser = RatingSerializer(data=request.data, context={'request': request})
